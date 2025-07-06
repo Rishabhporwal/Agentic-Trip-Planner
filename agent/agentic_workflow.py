20fff5bf50fc3b0572e277bcd45e3f4ebe0e7109
Rishabh Porwal <rishabhporwal95@gmail.com>
2025-07-06 16:53:04 +0530

    feature: tool calling is finished

--- agent/agentic_workflow.py
+++ agent/agentic_workflow.py
@@ -6,17 +6,32 @@ from langgraph.prebuilt import ToolNode, tools_condition
 
 from tools.weather_info_tool import WeatherInfoTool
 from tools.place_search_tool import PlaceSearchTool
-from tools.calculator_tool import CalculatorTool
+from tools.expense_calculator_tool import CalculatorTool
 from tools.currency_conversion_tool import CurrencyConverterTool
 
 class GraphBuilder():
-    def __init__(self):
-        self.tools = [
-            WeatherInfoTool(),
-            PlaceSearchTool(),
-            CalculatorTool(),
-            CurrencyConverterTool()
-        ]
+    def __init__(self, model_provider: str = "groq"):
+        self.model_loader = ModelLoader(model_provider=model_provider)
+        self.llm = self.model_loader.load_llm()
+
+        self.tools = []
+
+        self.weather_info_tool = WeatherInfoTool()
+        self.place_search_tool = PlaceSearchTool()
+        self.calculator_tool = CalculatorTool()
+        self.currency_converter_tool = CurrencyConverterTool()
+        
+        self.tools.extend([
+            * self.weather_info_tool.weather_tool_list,
+            * self.place_search_tool.place_search_tool_list,
+            * self.calculator_tool.calculator_tool_list,
+            * self.currency_converter_tool.currency_converter_tool_list
+        ])
+
+        self.llm_with_tools = self.llm.bind_tools(self.tools)
+
+
+        self.graph = None
 
         self.system_prompt = SYSTEM_PROMPT
 
