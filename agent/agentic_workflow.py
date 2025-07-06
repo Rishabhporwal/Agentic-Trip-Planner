e45f7edae6b9307e744ef0001db4267342acb6f7
Rishabh Porwal <rishabhporwal95@gmail.com>
2025-07-06 16:05:09 +0530

    feature: created endpoint and frontend with streamlit

--- agent/agentic_workflow.py
+++ agent/agentic_workflow.py
@@ -1,5 +1,5 @@
 from utils.model_loader import ModelLoader
-from prompt_library.prompt import System_Prompt
+from prompt_library.prompt import SYSTEM_PROMPT
 
 from langgraph.graph import StateGraph, MessagesState, END, START
 from langgraph.prebuilt import ToolNode, tools_condition
